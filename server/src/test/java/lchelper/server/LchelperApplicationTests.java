bdafc487b1c31e168034f6a92885d916c0ca6e56
tiaotiao <tiaotiaoyly@gmail.com>
2020-10-04 22:35:15 -0700

    Init server

---
+++ server/src/test/java/lchelper/server/LchelperApplicationTests.java
@@ -0,0 +1,13 @@
+package lchelper.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LchelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
